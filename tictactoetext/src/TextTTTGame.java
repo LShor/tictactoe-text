c808f0e0f9758fc5a9bbf8a33a5b7c8019be231a
LShor <lindsey.shorser@gmail.com>
2024-08-28 08:34:09 -0400

    Replaced while(true) with an exit condition based on user input.

--- tictactoetext/src/TextTTTGame.java
+++ tictactoetext/src/TextTTTGame.java
@@ -16,18 +16,19 @@ public class TextTTTGame {
         TicTacToeController controller = new TicTacToeController(useCase);
         Scanner scanner = new Scanner(System.in);
 
-        while (true) {
+        System.out.println("To exit the game at any time, type '9'. To start, type any other number.");
+        int userCommand = scanner.nextInt();
+
+        while (userCommand != 9) {
             view.displayBoard(board.getBoard());
-            System.out.println("Player " + board.getCurrentPlayer() + ", enter your move by typing the row then a space then column number. Your options are 0, 1, or 2. To exit, enter 9 and then 9");
+            System.out.println("Player " + board.getCurrentPlayer() + ", enter your move by typing the row then a space then column number. Your options are 0, 1, or 2.");
             int row = scanner.nextInt();
             int col = scanner.nextInt();
-            if (row==9 && col==9) {
-                break;
-            }
             controller.makeMove(row, col);
             if (board.checkWin() != '-' || board.isFull()) {
                 break;
             }
+            userCommand = row;
         }
 
         view.displayBoard(board.getBoard());
